b3780a409478752051505125589175f639d2c2e2
Wes Higbee <wes.mcclure@gmail.com>
2024-12-03 00:32:20 -0600

    notes that lead me to believe it is indeed a bug and one that appears fixed in latest rust builds of fish

--- tests/exec-file-sync.cjs
+++ tests/exec-file-sync.cjs
@@ -15,5 +15,20 @@ execFile("ls", ["-l"], { input: "echo foobar2" }, (error, stdout, stderr) => {
 const inbash = execFileSync("bash", [], { input: "echo foobar" });
 console.log(inbash.toString()); // sync means it will show before async callbacks
 const infish = execFileSync("fish", [], { input: "echo foobar" });
+// FYI did some testing of fish shell:
+//   new rust version doesn't fail on this, so this is definitely a bug in c++ builds only
+//   - reproduced with remote:  origin/Integration_3.7.1
+//     make 
+//     set PATH /Users/wes/repos/github/fish-shell/fish-shell/build  $PATH
+//     node exec-file-sync.cjs # this script
+//     FYI traced back to this line: https://github.com/fish-shell/fish-shell/blob/tmp3.7.1/src/reader.cpp#L4763
+//
+//   rust build works:
+//     gcob master
+//     cargo build
+//     set PATH /Users/wes/repos/github/fish-shell/fish-shell/target/debug/ $PATH
+//     node exec-file-sync.cjs # this script
+//     # works fine! perhaps I should start running the rust build :) and give feedback on bugs anyways?
+//   
 // show buffer text:
 console.log(infish.toString()); // sync means it will show before async callbacks
