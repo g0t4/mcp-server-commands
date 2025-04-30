26c73b9e37dc9d10542d9805e60b322f62f42a6e
Wes Higbee <wes.mcclure@gmail.com>
2025-04-30 06:23:58 -0500

    split out logging code

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import os from 'os'
+import os from "os";
 
 import { Server } from "@modelcontextprotocol/sdk/server/index.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
@@ -16,7 +16,7 @@ import { CallToolResult } from "@modelcontextprotocol/sdk/types.js";
 import { runCommand } from "./run-command.js";
 
 import { createRequire } from "module";
-import { always_log } from "./always_log.js";
+import { verbose_log } from "./always_log.js";
 const require = createRequire(import.meta.url);
 const {
     name: package_name,
@@ -25,12 +25,6 @@ const {
 
 // TODO use .promises? in node api
 const execAsync = promisify(exec);
-
-let verbose = false;
-// check CLI args:
-if (process.argv.includes("--verbose")) {
-    verbose = true;
-}
 
 const server = new Server(
     {
@@ -48,54 +42,14 @@ const server = new Server(
     }
 );
 
-if (verbose) {
-    always_log("INFO: verbose logging enabled");
-} else {
-    always_log("INFO: verbose logging disabled, enable it with --verbose");
-}
-
-function verbose_log(message: string, data?: any) {
-    // https://modelcontextprotocol.io/docs/tools/debugging - mentions various ways to debug/troubleshoot (including dev tools)
-    //
-    // remember STDIO transport means can't log over STDOUT (client expects JSON messages per the spec)
-    // https://modelcontextprotocol.io/docs/tools/debugging#implementing-logging
-    //   mentions STDERR is captured by the host app (i.e. Claude Desktop app)
-    //   server.sendLoggingMessage is captured by MCP client (not Claude Desktop app)
-    //   SO, IIUC use STDERR for logging into Claude Desktop app logs in:
-    //      '~/Library/Logs/Claude/mcp.log'
-    if (verbose) {
-        always_log(message, data);
-    }
-    // inspector, catches these logs and shows them on left hand side of screen (sidebar)
-
-    // TODO add verbose parameter (CLI arg?)
-
-    // IF I wanted to log via MCP client logs (not sure what those are/do):
-    //  I do not see inspector catching these logs :(, there is a server notifications section and it remains empty
-    //server.sendLoggingMessage({
-    //    level: "info",
-    //    data: message,
-    //});
-    // which results in something like:
-    //server.notification({
-    //    method: "notifications/message",
-    //    params: {
-    //        level: "warning",
-    //        logger: "mcp-server-commands",
-    //        data: "ListToolsRequest2",
-    //    },
-    //});
-    //
-    // FYI client should also requets a log level from the server, so that needs to be here at some point too
-}
-
 server.setRequestHandler(ListToolsRequestSchema, async () => {
     verbose_log("INFO: ListTools");
     return {
         tools: [
             {
                 name: "run_command",
-                description: "Run a command on this " + os.platform() + " machine",
+                description:
+                    "Run a command on this " + os.platform() + " machine",
                 inputSchema: {
                     type: "object",
                     properties: {
