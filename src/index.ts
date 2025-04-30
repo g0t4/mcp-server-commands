5a0a4a12deaac02add27a15dc9e2a1f82a35a9f8
Wes Higbee <wes.mcclure@gmail.com>
2025-04-30 06:37:21 -0500

    split out tools registrations too

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,7 @@ import { runCommand } from "./run-command.js";
 import { createRequire } from "module";
 import { verbose_log } from "./always_log.js";
 import { registerPrompts } from "./prompts.js";
+import { reisterTools } from "./tools.js";
 const require = createRequire(import.meta.url);
 const {
     name: package_name,
@@ -35,55 +36,7 @@ const server = new Server(
     }
 );
 
-server.setRequestHandler(ListToolsRequestSchema, async () => {
-    verbose_log("INFO: ListTools");
-    return {
-        tools: [
-            {
-                name: "run_command",
-                description:
-                    "Run a command on this " + os.platform() + " machine",
-                inputSchema: {
-                    type: "object",
-                    properties: {
-                        command: {
-                            type: "string",
-                            description: "Command with args",
-                        },
-                        workdir: {
-                            // previous run_command calls can probe the filesystem and find paths to change to
-                            type: "string",
-                            description: "Optional, current working directory",
-                        },
-                        stdin: {
-                            type: "string",
-                            description:
-                                "Optional, text to pipe into the command's STDIN. For example, pass a python script to python3. Or, pass text for a new file to the cat command to create it!",
-                        },
-                        // args to consider:
-                        // - env - obscure cases where command takes a param only via an env var?
-                        // - timeout - lets just hard code this for now
-                    },
-                    required: ["command"],
-                },
-            },
-        ],
-    };
-});
-
-server.setRequestHandler(
-    CallToolRequestSchema,
-    async (request): Promise<CallToolResult> => {
-        verbose_log("INFO: ToolRequest", request);
-        switch (request.params.name) {
-            case "run_command": {
-                return await runCommand(request.params.arguments);
-            }
-            default:
-                throw new Error("Unknown tool");
-        }
-    }
-);
+reisterTools(server);
 
 registerPrompts(server);
 
