d3146374ff735f7accaaed112cb5e5c2445ccd40
Wes Higbee <wes.mcclure@gmail.com>
2026-01-24 15:02:56 -0600

    duplicate

--- src/decide/spawn.ts
+++ src/decide/spawn.ts
@@ -23,3 +23,24 @@ async function ls() {
 
 await ls()
 
+
+async function cat_hello() {
+    const ls = spawn('ls', ['-lh', '/usr']);
+
+    // NOTES:
+    // - spawn/spawnSync are the foundation of all other methods
+    // - async, non-blocking (event loop)
+
+    ls.stdout.on('data', (data) => {
+        console.log(`stdout: ${data}`);
+    });
+
+    ls.stderr.on('data', (data) => {
+        console.error(`stderr: ${data}`);
+    });
+
+    const [code] = await once(ls, 'close');
+    console.log(`child process exited with code ${code}`);
+}
+
+await cat_hello()
