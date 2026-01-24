389bd746b92ea53aedcced93ec18aac6da45e9b4
Wes Higbee <wes.mcclure@gmail.com>
2026-01-24 15:02:15 -0600

    nested func for scenario

--- src/decide/spawn.ts
+++ src/decide/spawn.ts
@@ -1,18 +1,25 @@
-import {spawn} from 'node:child_process';
-import {once} from 'node:events';
-const ls = spawn('ls', ['-lh', '/usr']);
+import { spawn } from 'node:child_process';
+import { once } from 'node:events';
 
-// NOTES:
-// - spawn/spawnSync are the foundation of all other methods
-// - async, non-blocking (event loop)
 
-ls.stdout.on('data', (data) => {
-    console.log(`stdout: ${data}`);
-});
+async function ls() {
+    const ls = spawn('ls', ['-lh', '/usr']);
 
-ls.stderr.on('data', (data) => {
-    console.error(`stderr: ${data}`);
-});
+    // NOTES:
+    // - spawn/spawnSync are the foundation of all other methods
+    // - async, non-blocking (event loop)
 
-const [code] = await once(ls, 'close');
-console.log(`child process exited with code ${code}`);
+    ls.stdout.on('data', (data) => {
+        console.log(`stdout: ${data}`);
+    });
+
+    ls.stderr.on('data', (data) => {
+        console.error(`stderr: ${data}`);
+    });
+
+    const [code] = await once(ls, 'close');
+    console.log(`child process exited with code ${code}`);
+}
+
+await ls()
+
