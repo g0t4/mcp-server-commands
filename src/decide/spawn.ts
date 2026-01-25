37c5cea8898a937bee932a8aeca60a36c196ae4b
Wes Higbee <wes.mcclure@gmail.com>
2026-01-24 18:00:12 -0600

    tinker w/ AbortController

--- src/decide/spawn.ts
+++ src/decide/spawn.ts
@@ -1,4 +1,4 @@
-import { spawn } from 'node:child_process';
+import { spawn, SpawnOptions } from 'node:child_process';
 import { once } from 'node:events';
 
 
@@ -20,8 +20,7 @@ async function ls() {
     const [code] = await once(ls, 'close');
     console.log(`child process exited with code ${code}`);
 }
-
-await ls()
+// await ls();
 
 
 async function cat_hello() {
@@ -40,5 +39,37 @@ async function cat_hello() {
     const [code] = await once(cat, 'close');
     console.log(`child process exited with code ${code}`);
 }
+// await cat_hello();
 
-await cat_hello()
+
+async function sleep_abort() {
+    const aborter = new AbortController();
+    const options: SpawnOptions = {};
+    // options.shell = ""
+    options.signal = aborter.signal;
+    const cat = spawn('sleep', ["5"], options);
+
+    if (cat.stdout) {
+        cat.stdout.on('data', (data) => {
+            console.log(`stdout: ${data}`);
+        });
+    }
+
+    if (cat.stderr) {
+        cat.stderr.on('data', (data) => {
+            console.error(`stderr: ${data}`);
+        });
+    }
+
+    cat.on('error', (err) => {
+        console.log("ERR", err);
+    });
+    aborter.abort();
+
+    const [code] = await once(cat, 'close');
+    console.log(`child process exited with code ${code}`);
+}
+
+await sleep_abort();
+
+// TODO try shell vs not shell (provide custom shell too)
