0251c659031bbf82e29226e659196025f1686d0d
Wes Higbee <wes.mcclure@gmail.com>
2026-01-24 23:16:13 -0600

    partial port of run_command to run_process, within prompts

--- src/prompts.ts
+++ src/prompts.ts
@@ -16,29 +16,23 @@ export function registerPrompts(server: Server) {
         verbose_log("INFO: ListPrompts");
         return {
             prompts: [
-                // TODO! add prompts for various LLMs that tailor instructions to make them optimize use of run_command tool
+                // ?? add prompts for various LLMs that tailor instructions to make them optimize use of run_command tool
                 //  idea is, users could insert those manually, or perhaps automatically if necessary, depending on context
                 //  that way you don't need one prompt for everything and certain models won't need any help (i.e. Claude) vs
                 //  llama4 which struggled with old run_script tool (now just stdin on run_command) so it might need some
                 //  special instructions and yeah... I think that's a use case for these prompts
                 //  /prompt llama4 ?
                 {
-                    name: "examples",
-                    description:
-                        "Novel examples of run_command tool use to nudge models to the possibilities. " +
-                        "Based on assumption that most models understand shell commands/scripts very well.",
-                },
-                {
-                    name: "run_command",
+                    name: "run_process",
                     description:
                         "Include command output in the prompt. " +
                         "This is effectively a user tool call.",
                     arguments: [
-                        {
-                            name: "command",
-                            required: true,
-                        },
-                        // if I care to keep the prompt tools then add stdin?
+                        { name: "mode", },
+                        { name: "commandLine", },
+                        { name: "argv", },
+                        { name: "dryRun", },
+                        // ? other args?
                     ],
                 },
             ],
@@ -49,24 +43,25 @@ export function registerPrompts(server: Server) {
         // if (request.params.name == "examples") {
         //     return GetExamplePromptMessages();
         // } else
-        if (request.params.name !== "run_command") {
+        if (request.params.name !== "run_process") {
             throw new Error(
                 "Unknown or not implemented prompt: " + request.params.name
             );
         }
         verbose_log("INFO: PromptRequest", request);
+        throw new Error("run_process not yet ported to prompts");
 
-        const command = String(request.params.arguments?.command);
-        if (!command) {
-            // TODO is there a format to follow for reporting failure like isError for tools?
-            throw new Error("Command is required");
-        }
+        const commandLine = String(request.params.arguments?.commandLine);
+        const argv = String(request.params.arguments?.argv);
+        const mode = String(request.params.arguments?.mode);
+        const dryRun = Boolean(request.params.arguments?.dryRun);
         // Is it possible/feasible to pass a path for the workdir when running the command?
         // - currently it uses / (yikez)
         // - IMO makes more sense to have it be based on the Zed workdir of each project
         // - Fallback could be to configure on server level (i.e. home dir of current user) - perhaps CLI arg? (thinking of zed's context_servers config section)
 
-        const { stdout, stderr } = await execAsync(command);
+        // TODO! finish rest of migrtation to run_process
+        const { stdout, stderr } = await execAsync(commandLine);
         // TODO gracefully handle errors and turn them into a prompt message that can be used by LLM to troubleshoot the issue, currently errors result in nothing inserted into the prompt and instead it shows the Zed's chat panel as a failure
 
         const messages: PromptMessage[] = [
@@ -76,7 +71,7 @@ export function registerPrompts(server: Server) {
                     type: "text",
                     text:
                         "I ran the following command, if there is any output it will be shown below:\n" +
-                        command,
+                        commandLine,
                 },
             },
         ];
