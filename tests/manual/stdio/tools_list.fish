c991d54b5aa7b4db6202e144665a2ad96942cbff
Wes Higbee <wes.mcclure@gmail.com>
2026-04-01 00:11:20 -0500

    setup script to test a JSON-RPC request over STDIO

---
+++ tests/manual/stdio/tools_list.fish
@@ -0,0 +1,13 @@
+#!/usr/bin/env fish
+set request '{ "jsonrpc": "2.0", "id": 1, "method": "tools/list" }'
+
+echo $request \
+    | npx ~/repos/github/g0t4/mcp-server-commands/build/index.js \
+    | jq
+
+# ?? make this into an integration test that I can run to verify STDIO comms?
+
+# FYI original goal (for these scripts) was to make executable a few sanity checks of JSON-RPC over STDIO
+#   - also helps me remember how things work
+#   - and then I can spin this off into troubleshooting reproductions 
+#     - i.e. response object seemingly split across lines... though might not be from this server
