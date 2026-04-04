c3e96fe3dc33c645cd3ba7eaf55c5b1286af8821
Wes Higbee <wes.mcclure@gmail.com>
2026-04-04 15:19:15 -0500

    notes

--- src/logging.ts
+++ src/logging.ts
@@ -12,45 +12,40 @@ if (is_verbose) {
 }
 
 export function verbose_log(message: string, data?: any) {
-    // https://modelcontextprotocol.io/docs/tools/debugging - mentions various ways to debug/troubleshoot (including dev tools)
-    //
-    // remember STDIO transport means can't log over STDOUT (client expects JSON messages per the spec)
-    // https://modelcontextprotocol.io/docs/tools/debugging#implementing-logging
-    //   mentions STDERR is captured by the host app (i.e. Claude Desktop app)
-    //   server.sendLoggingMessage is captured by MCP client (not Claude Desktop app)
-    //   * SO, IIUC use STDERR for logging into Claude Desktop app logs in:
-    //      '~/Library/Logs/Claude/mcp.log'
     if (is_verbose) {
         always_log(message, data);
     }
-    // inspector, catches these logs and shows them on left hand side of screen (sidebar)
-
-    // IF I wanted to log via MCP client logs (not sure what those are/do):
-    //  I do not see inspector catching these logs :(, there is a server notifications section and it remains empty
-    //server.sendLoggingMessage({
-    //    level: "info",
-    //    data: message,
-    //});
-    // which results in something like:
-    //server.notification({
-    //    method: "notifications/message",
-    //    params: {
-    //        level: "warning",
-    //        logger: "mcp-server-commands",
-    //        data: "ListToolsRequest2",
-    //    },
-    //});
-    //
-    // FYI client should also requets a log level from the server, so that needs to be here at some point too
 }
 
 export function always_log(message: string, data?: any) {
+
+    // LOGGING NOTES:
+    // https://modelcontextprotocol.io/docs/tools/debugging#implementing-logging
+    // * STDIO transport => STDERR captured by host app (i.e. Claude Desktop which writes to ~/Library/Logs/Claude/mcp.log)
+    // * Streamable HTTP transport => suggests server side capture (i.e. log file)
+    // * and/or log to MCP client regardless of transport
+    //   server.sendLoggingMessage({
+    //       level: "info",
+    //       data: message,
+    //   });
+    //   // sends a message like:
+    //   server.notification({
+    //      method: "notifications/message",
+    //      params: {
+    //          level: "warning",
+    //          logger: "mcp-server-commands",
+    //          data: "ListToolsRequest2",
+    //      },
+    //   });
+
     const isJest = typeof process !== 'undefined' && !!process.env.JEST_WORKER_ID;
     if (isJest) {
+        // * JEST => log to console so I can see in test runner output
         console.log(`${message}${data ? ": " + JSON.stringify(data) : ""}`);
         return;
     }
 
+    // * all transports => server side log file
     const timestamp = new Date().toISOString();
     const logMessage = `[${timestamp}] ${message}${data ? ": " + JSON.stringify(data) : ""}`;
     const shareDir = process.env.HOME + "/.local/share/mcp-server-commands/";
