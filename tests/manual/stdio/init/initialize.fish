2dd01e68f104b2c84f41f7ed07a5dfe0d5ab8b1e
Wes Higbee <wes.mcclure@gmail.com>
2026-04-06 02:49:59 -0500

    tests fetch server init too

--- tests/manual/stdio/init/initialize.fish
+++ tests/manual/stdio/init/initialize.fish
@@ -4,10 +4,12 @@
 # - but for newer protocol revisions, the SDK expects this else it will fail with:
 #   WARNING:root:Failed to validate request: Received request before initialization was complete
 
-set request '{"jsonrpc":"2.0","id":1,"method":"initialize","params":{"protocolVersion":"2024-11-05","capabilities":{},"clientInfo":{"name":"test","version":"1.0"}}}'; \
+set request '{"jsonrpc":"2.0","id":1,"method":"initialize","params":{"protocolVersion":"2024-11-05","capabilities":{},"clientInfo":{"name":"test","version":"1.0"}}}'
 
 echo $request \
     | npx ~/repos/github/g0t4/mcp-server-commands/build/index.js -- --verbose  \
     | jq
 
-
+echo $request \
+    | docker container run -i --rm mcp/fetch \
+    | jq
