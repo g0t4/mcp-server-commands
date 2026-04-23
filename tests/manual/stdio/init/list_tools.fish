041519f724eeb0b20751439fa27f042a2e7b5431
Wes Higbee <wes.mcclure@gmail.com>
2026-04-23 01:22:20 -0500

    comment out

--- tests/manual/stdio/init/list_tools.fish
+++ tests/manual/stdio/init/list_tools.fish
@@ -9,18 +9,26 @@ set request '{"jsonrpc":"2.0","id":1,"method":"initialize","params":{"protocolVe
 # initialized notification DOES NOT HAVE ID
 set initialized '{"jsonrpc":"2.0","method":"notifications/initialized"}'
 
-begin
-    echo $request
-    sleep 1
-    echo $initialized
-    sleep 1
-end | npx ~/repos/github/g0t4/mcp-server-commands/build/index.js -- --verbose \
-    | jq
+set list_tools '{"jsonrpc":"2.0","id":2,"method":"tools/list"}'
 
+# * mcp-server-commands
+# begin
+#     echo $request
+#     sleep 1
+#     echo $initialized
+#     sleep 1
+#     echo $list_tools
+#     sleep 1
+# end | npx ~/repos/github/g0t4/mcp-server-commands/build/index.js -- --verbose \
+#     | jq
+
+# * fetch
 begin
     echo $request
     sleep 1
     echo $initialized
+    sleep 1
+    echo $list_tools
     sleep 1
 end | uvx --directory ~/repos/github/g0t4/mcp-servers/src/fetch mcp-server-fetch \
     # | docker container run -i --rm mcp/fetch \
